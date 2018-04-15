155b346a76377cba2a2a5f5ede642205111c9cc2
Steven Turmel <steven.turmel@gmail.com>
2018-04-15 11:09:21 -0400

    Finished authentication class

--- src/server/AuthenticationHandler.java
+++ src/server/AuthenticationHandler.java
@@ -1,10 +1,14 @@
 package server;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 
 import static server.AccessController.DEFAULT_NUM_FILES;
-import
 
 public class AuthenticationHandler {
     private String prefix = "C://temp//";
@@ -18,7 +22,7 @@ public class AuthenticationHandler {
         byte[] addedHash = addBytes(username.getBytes(), hashpass);
         Integer checksum = hashpass.hashCode();
         int filecheck = checksum%DEFAULT_NUM_FILES;
-        return checkFile(filecheck);
+        return checkFile(filecheck, addedHash);
     }
 
     private byte[] addBytes(byte[] arr1, byte[] arr2) {
@@ -34,7 +38,31 @@ public class AuthenticationHandler {
         return out;
     }
 
-    private boolean checkFile(int i) {
-
+    private boolean checkFile(int index, byte[] checksum) {
+        try {
+            File loadedFile = new File(prefix + filename.getNames(index));
+            InputStream targetStream = FileUtils.openInputStream(loadedFile);
+            byte[] filecontents = IOUtils.toByteArray(targetStream);
+            for (int i = 0; i < filecontents.length; i+=128) {
+                byte[] temp = splitBytes(filecontents, i);
+                if (temp == checksum)
+                    return true;
+                else
+                    ;
+            }
+        } catch (IOException e) {
+            System.out.println("Error Loading File");
+            e.printStackTrace();
+        }
+        return false;
     }
+
+    private byte[] splitBytes(byte[] b, int initial) {
+        byte[] out = new byte[128];
+        for (int i = 0; i < 128; i++, initial++) {
+            out[i] = b[initial];
+        }
+        return out;
+    }
+
 }
